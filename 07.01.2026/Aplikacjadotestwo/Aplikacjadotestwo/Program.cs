9e1b1b1361e189c5be4256e0519f13f0cdaa03fb
PS4Repairman <mail@mail.com>
2026-01-26 15:35:18 +0100

    123

---
+++ 07.01.2026/Aplikacjadotestwo/Aplikacjadotestwo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static double Oblicz(int[] liczby) 
+    { 
+        int sum = liczby.Sum();
+
+        if (sum < 10)
+        {
+            return liczby.Average();
+        }
+        else if (sum >= 11 && sum <= 20)
+        {
+            return sum;
+        }else if (sum > 20 )
+            return liczby.Where(x => x % 2 !=0).Sum();
+    }
+
+    static void Main()
+    {
+        int[] liczby = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) 
+        {
+            liczby[i] = random.Next(0,5);
+        }
+        Console.WriteLine("Oto wylosowaneliczby");
+        foreach(int x in liczby) 
+            Console.Write(x + " ");
+        Console.WriteLine();
+
+        double wynik = Oblicz(liczby);
+        Console.WriteLine("Wynik" + wynik);
+
+    }
+}
---
+++ 07.01.2026/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace program
+{
+    class Uczen
+    {
+        private string imie;
+        private string nazwisko;
+        private double sredniaOcen;
+
+        public Uczen(string imie, string nazwisko, double sredniaOcen)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.sredniaOcen = sredniaOcen;
+        }
+        public bool CzyZdany()
+        {
+            return sredniaOcen >= 2.0;
+        }
+
+        public void WyswietlDane()
+        {
+            Console.WriteLine(imie + " " + nazwisko);
+            Console.WriteLine(imie + " " + nazwisko);
+        }
+    }
+}
